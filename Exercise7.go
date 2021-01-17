30b2bfd4b940188a9fda23a60ccb5c4274db5b5e
Akshay Bhansali <abhansal@abhansal.remote.csb>
2021-01-17 17:08:36 +0530

    Exercise 7 completed

---
+++ Exercise7.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+url:= "http://www.google.com/robots.txt"
+channel:= make(chan string)
+go read(channel, url)
+fmt.Println(<-channel)
+}
+func read(c chan string,url string) {
+	res, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	robots, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	c<-string(robots)
+
+}
